84d9238346371b4d57d8ee7138a1bd71de473d2b
Aurelien <aureliendardant@hotmail.fr>
2018-10-19 15:31:25 +0200

    design2

--- index.php
+++ index.php
@@ -1,64 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+  <head>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <title>Shop Homepage - Start Bootstrap Template</title>
+    <title>Shop Homepage - Start Bootstrap Template</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- Custom styles for this template -->
-  <link href="css/shop-homepage.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/shop-homepage.css" rel="stylesheet">
 
-</head>
+  </head>
 
-<body>
+  <body>
 
-  <!-- Navigation -->
+    <!-- Navigation -->
 
-  <!-- Page Content -->
-  <div class="container" style="margin-top: 20px">
+    <!-- Page Content -->
+    <div class="container" style="margin-top: 20px">
 
-    <div class="row">
+      <div class="row">
 
-      <div class="col-lg-3">
+        <div class="col-lg-3">
 
-        <div class="list-group">
-          <button type="button" href="epicerie.php" class="btn btn-outline-primary mb-3">EPICERIE</button>
-          <button type="button"  href="surgeles.php" class="btn btn-outline-primary mb-3">SUREGELÉS</a>
-            <button type="button"  href="viande.php" class="btn btn-outline-primary mb-3">VIANDE/POISSON</a>
-              <button type="button"  href="liste.php" class="btn btn-outline-primary mb-3">LISTE DE COURSE</a>
-              </div>
-
-            </div>
-            <!-- /.col-lg-3 -->
-
-            <div class="col-lg-9">
-
-
-
-              <!-- /.row -->
-
-            </div>
-            <!-- /.col-lg-9 -->
-
+          <div class="list-group">
+            <a href="epicerie.php" class="list-group-item list-group-item-action list-group-item-primary">EPICERIE</a>
+            <a href="surgeles.php" class="list-group-item list-group-item-action list-group-item-dark">SUREGELÉS</a>
+            <a href="viande.php" class="list-group-item list-group-item-action list-group-item-primary">VIANDE/POISSON</a>
+            <a href="liste.php" class="list-group-item list-group-item-action list-group-item-dark">LISTE DE COURSE</a>
           </div>
+
+        </div>
+        <!-- /.col-lg-3 -->
+
+        <div class="col-lg-9">
+
+          <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+              <div class="carousel-item active">
+                <img class="d-block img-fluid" src="http://www.e-leclerc.com/objetsportail/sites/portail/files/panonceaux/1391/Actualité/Magasin/offre%205%25%201920x1080.jpg" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="http://www.e-leclerc.com/objetsportail/sites/portail/files/panonceaux/1391/Actualité/Magasin/offre%205%25%201920x1080.jpg" alt="Second slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="http://www.e-leclerc.com/objetsportail/sites/portail/files/panonceaux/1391/Actualité/Magasin/offre%205%25%201920x1080.jpg" alt="Third slide">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
           <!-- /.row -->
 
         </div>
-        <!-- /.container -->
+        <!-- /.col-lg-9 -->
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
 
 
-        <!-- Bootstrap core JavaScript -->
-        <script src="vendor/jquery/jquery.min.js"></script>
-        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-      </body>
+  </body>
 
-      </html>
+</html>
