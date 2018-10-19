dbe959c4099ac3e2da05267ff7a683fcbdf4be4f
Aurelien <aureliendardant@hotmail.fr>
2018-10-19 12:59:04 +0200

    retour home

--- epicerie.php
+++ epicerie.php
@@ -23,7 +23,7 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#">Epicerie</a>
+        <a class="navbar-brand" href="index.php">Home</a>
         
       </div>
     </nav>
